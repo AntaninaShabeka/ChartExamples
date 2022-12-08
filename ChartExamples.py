26f29a7be7ec81b955a155ac15a5ee194d40eda5
AntaninaShabeka <zemlianika@gmail.com>
2022-12-08 09:13:16 +0300

    Добавление кнопок и меток для графиков 1, 2, диаграммы 1

--- ChartExamples.py
+++ ChartExamples.py
@@ -4,6 +4,8 @@ import tkinter as tk
 
 #Импорт внешних файлов
 import chart1
+import chart2
+import chart3
 
 # Функция закрытия программы
 def do_close():
@@ -18,12 +20,26 @@ window.title("Примеры построения графиков")
 lblTitle = tk.Label(text = "Примеры построения графиков", font = ('Helvetica', 18, 'bold'), fg = '#6600CC')
 lblTitle.place(x=25, y=35)
 
-# Добавление кномки и метки для графика 1
+# Добавление кнопки и метки для графика 1
 btnChart1 = tk.Button(window, text="График 1", font = ('Helvetica', 10, 'bold'), command=chart1.plot_chart)
 btnChart1.place(x=30, y=100, width=120, height=40)
 
-lblChart1 = tk.Label(text = "График синуса matplotlib", font = ('Helvetica', 14), fg = '#6600CC')
+lblChart1 = tk.Label(text = "График синуса matplotlib", font = ('Helvetica', 10), fg = '#6600CC')
 lblChart1.place(x=200, y=100)
+
+# Добавление кнопки и метки для диаграммы 1
+btnChart1 = tk.Button(window, text="Диаграмма 1", font = ('Helvetica', 10, 'bold'), command=chart2.plot_chart)
+btnChart1.place(x=30, y=150, width=120, height=40)
+
+lblChart1 = tk.Label(text = "Горизонтальная столбчатая диаграмма", font = ('Helvetica', 10), fg = '#6600CC')
+lblChart1.place(x=200, y=150)
+
+# Добавление кнопки и метки для графика 2
+btnChart1 = tk.Button(window, text="График 2", font = ('Helvetica', 10, 'bold'), command=chart3.plot_chart)
+btnChart1.place(x=30, y=200, width=120, height=40)
+
+lblChart1 = tk.Label(text = "График нормального распределения", font = ('Helvetica', 10), fg = '#6600CC')
+lblChart1.place(x=200, y=200)
 
 # Добавление кнопки закрытия программы
 btnClose = tk.Button(window, text="Закрыть", font = ('Helvetica', 10, 'bold'), command=do_close)
